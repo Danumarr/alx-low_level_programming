ad185eab2ee27489414d3d237802a80b514ecd88
Danumarr <danumar019@gmail.com>
2022-09-16 07:55:42 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
+
 /**
- *  main - Entry point
- *  Description: prints "\"Programming is like building a multilingual puzzle"
- *  Return: 0
+ *  main - Printing
+ *
+ *  Return: Always 0 (Successful)
  */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-	 	return (0);
-
+     puts("\"Programming is like building a multilingual
+puzzle");
+     return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Print a string
- * Description: use printf
- * Return: 0
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-
-		return (0);
+     printf("with proper grammar, but the outcome is a piece of 
+art,\n");
+     return (0);
 }
